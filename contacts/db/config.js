c4f3172ed5e4becb1090ee5ee5c2353d4fd9076b
Luis Cacho <luiscachog@gmail.com>
2021-03-06 23:03:05 -0600

    updated app to v2

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
-const { Pool } = require('pg');
+require("dotenv").config();
+const DB_CONFIG =
+  process.env.DB_CONFIG ||
+  "postgresql://contacts:contacts@localhost:5432/contacts";
+const { Pool } = require("pg");
 
 const pgconn = new Pool({
-    connectionString: DB_CONFIG,
-    ssl: false,
+  connectionString: DB_CONFIG,
+  ssl: false,
 });
-  
-module.exports = { pgconn }
+
+module.exports = { pgconn };
